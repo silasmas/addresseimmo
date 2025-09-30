b15c352937ff710970c3eddadd429f53dd90fc2c
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2025-09-30 14:41:54 +0100

    Update datas.blade.php

--- resources/views/partials/products/datas.blade.php
+++ resources/views/partials/products/datas.blade.php
@@ -22,7 +22,7 @@
 
         <div class="section">
             <div class="container">
-                <div class="row">
+                <div class="row g-4">
                     <div class="col-lg-7">
                         <div class="img-property-slide-wrap">
                             <div class="img-property-slide">
