9f8943507f6622a1f2dcae7c35a17ee1d2818ad9
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2025-09-30 13:33:25 +0100

    Update datas.blade.php

--- resources/views/partials/products/datas.blade.php
+++ resources/views/partials/products/datas.blade.php
@@ -35,46 +35,19 @@
                         </div>
                     </div>
                     <div class="col-lg-4">
-                        <h2 class="heading text-primary">5232 California Ave. 21BC</h2>
-                        <p class="meta">California, United States</p>
-                        <p class="text-black-50">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
-                            laborum quo quos omnis sed magnam id, ducimus saepe, debitis error
-                            earum, iste dicta odio est sint dolorem magni animi tenetur.
-                        </p>
-                        <p class="text-black-50">
-                            Perferendis eligendi reprehenderit, assumenda molestias nisi eius
-                            iste reiciendis porro tenetur in, repudiandae amet libero.
-                            Doloremque, reprehenderit cupiditate error laudantium qui, esse
-                            quam debitis, eum cumque perferendis, illum harum expedita.
-                        </p>
+                        <h2 class="heading text-primary">{{ $selected_product->product_name }}</h2>
+                        <p class="meta">{{ $selected_product->address }}</p>
+                        <p class="text-black-50">{{ $selected_product->product_description }}</p>
 
                         <div class="d-block agent-box p-5">
                             <div class="img mb-4">
-                                <img src="../../images/person_2-min.jpg" alt="Image" class="img-fluid" />
+                                <img src="{{ $selected_product->user->avatar_url }}" alt="Image" class="img-fluid" />
                             </div>
 
                             <div class="text">
-                                <h3 class="mb-0">Alicia Huston</h3>
-                                <div class="meta mb-3">Real Estate</div>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                    Ratione laborum quo quos omnis sed magnam id ducimus saepe
-                                </p>
-                                <ul class="list-unstyled social dark-hover d-flex">
-                                    <li class="me-1">
-                                        <a href="#"><span class="icon-instagram"></span></a>
-                                    </li>
-                                    <li class="me-1">
-                                        <a href="#"><span class="icon-twitter"></span></a>
-                                    </li>
-                                    <li class="me-1">
-                                        <a href="#"><span class="icon-facebook"></span></a>
-                                    </li>
-                                    <li class="me-1">
-                                        <a href="#"><span class="icon-linkedin"></span></a>
-                                    </li>
-                                </ul>
+                                <h2 class="h2 mb-0 fw-bold">{{ $selected_product->user->firstname . ' ' . $selected_product->user->lastname }}</h2>
+                                <div class="meta mb-3">{{ $selected_product->user->selected_role->user->role_name }}</div>
+                                {{-- <p>{{ $selected_product->user->avatar_url }}</p> --}}
                             </div>
                         </div>
                     </div>
