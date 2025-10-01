26c19144ebd690831bae06f917145b1dc642c613
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2025-10-01 07:24:20 +0100

    Update datas.blade.php

--- resources/views/partials/products/datas.blade.php
+++ resources/views/partials/products/datas.blade.php
@@ -56,8 +56,8 @@
     @else
         @if ($selected_product['quantity'] > 0)
                         <div id="addToCart-{{ $selected_product['id'] }}" class="mt-4 mb-0 d-flex flex-row align-items-center">
-                            <button class="btn btn-lg adrm-bg-green-transparent me-2 item-add-btn" data-id="{{ $selected_product['id'] }}"><i class="bi bi-cart3 me-2"></i>Ajouter au panier</button>
-                            <img id="ajax-loading-{{ $selected_product['id'] }}" class="d-non" src="{{ asset('assets/img/ajax-loading.gif') }}" alt="@lang('miscellaneous.loading')" width="30" height="30">
+                            <button class="btn btn-lg adrm-bg-red me-2 item-add-btn" data-id="{{ $selected_product['id'] }}"><i class="bi bi-cart3 me-2"></i>Ajouter au panier</button>
+                            <img id="ajax-loading-{{ $selected_product['id'] }}" class="d-none" src="{{ asset('assets/img/ajax-loading.gif') }}" alt="@lang('miscellaneous.loading')" width="30" height="30">
                         </div>
         @else
                         <p class="mt-4 mb-0">
