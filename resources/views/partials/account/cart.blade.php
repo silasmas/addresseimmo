8ce910be28378a7bd96b3cd3073268092c13e4ef
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2025-10-01 09:47:04 +0100

    Update cart.blade.php

--- resources/views/partials/account/cart.blade.php
+++ resources/views/partials/account/cart.blade.php
@@ -29,13 +29,13 @@
                                             <td class="text-center">{{ $item['converted_price_at_that_time'] . ' ' . $current_user['readable_currency'] }}</td>
                                             <td>
 												<div class="d-flex flex-row">
-													<input type="text" name="quantity" id="order-quantity-{{ $item['id'] }}" class="form-control" value="{{ $item['quantity'] }}" onchange="updateProductQuantity('update', {{ $item['id'] }}, this.value)" style="width: 100px;">
+													<input type="text" name="quantity" id="order-quantity-{{ $item['id'] }}" class="form-control" value="{{ $item['quantity'] }}" onchange="updateProductQuantity('update', {{ $item['id'] }}, this.value)" style="width: 80px;">
                                                     <div>
-                                                        <a role="button" class="btn btn-secondary p-2 pb-0 rounded-circle text-primary" onclick="event.preventDefault(); updateProductQuantity('increment', {{ $item['id'] }});">
+                                                        <a role="button" class="btn btn-secondary p-2 pb-0" onclick="event.preventDefault(); updateProductQuantity('increment', {{ $item['id'] }});">
                                                             <i class="fa fa-angle-up"></i>
                                                         </a>
-		@if ($item['quantity'] > 500)
-                                                        <a role="button" class="btn btn-secondary p-2 pb-0 rounded-circle text-primary" onclick="event.preventDefault(); updateProductQuantity('decrement', {{ $item['id'] }});">
+		@if ($item['quantity'] > 0)
+                                                        <a role="button" class="btn btn-secondary p-2 pb-0" onclick="event.preventDefault(); updateProductQuantity('decrement', {{ $item['id'] }});">
                                                             <i class="fa fa-angle-down"></i>
                                                         </a>
 		@endif
