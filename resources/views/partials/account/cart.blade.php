47ba91ba240b61bf877c51ae2fd62359440dc87a
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2025-10-01 10:54:32 +0100

    Update cart.blade.php

--- resources/views/partials/account/cart.blade.php
+++ resources/views/partials/account/cart.blade.php
@@ -37,12 +37,12 @@
 													<input type="text" name="quantity" id="order-quantity-{{ $item['id'] }}" class="form-control text-center" value="{{ $item['quantity'] }}" onchange="updateProductQuantity('update', {{ $item['id'] }}, this.value)" style="width: 80px;">
                                                     <div class="d-flex flex-column">
         @if ($item['product']['quantity'] > 0)
-                                                        <a role="button" class="btn btn-secondary px-2 pt-1 pb-0" onclick="event.preventDefault(); document.getElementById('ajaxLoader').classList.remove('ajaxLoader'); updateProductQuantity('increment', {{ $item['id'] }});">
+                                                        <a role="button" class="btn btn-secondary px-2 pt-1 pb-0" onclick="event.preventDefault(); document.getElementById('ajaxLoader').classList.remove('d-none'); updateProductQuantity('increment', {{ $item['id'] }});">
                                                             <i class="fa fa-plus"></i>
                                                         </a>
         @endif
 		@if ($item['quantity'] > 1)
-                                                        <a role="button" class="btn btn-secondary px-2 pt-1 pb-0" onclick="event.preventDefault(); document.getElementById('ajaxLoader').classList.remove('ajaxLoader'); updateProductQuantity('decrement', {{ $item['id'] }});">
+                                                        <a role="button" class="btn btn-secondary px-2 pt-1 pb-0" onclick="event.preventDefault(); document.getElementById('ajaxLoader').classList.remove('d-none'); updateProductQuantity('decrement', {{ $item['id'] }});">
                                                             <i class="fa fa-minus"></i>
                                                         </a>
 		@endif
